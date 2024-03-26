9016cfa221d4618aea2daed5eeeb925b02511e5e
aaa <13481468785@163.com>
2024-03-26 14:50:04 +0800

    泛型改造

--- SliceOperate/SliceDel.go
+++ SliceOperate/SliceDel.go
@@ -1,19 +1,21 @@
 package SliceOperate
 
-import "fmt"
-
-func SliceDel(index int, s []int) []int {
+func SliceDel[T collect](index int, s []T) []T {
 	println("传入容量:", cap(s), "传入长度:", len(s))
 	rs := s[:index]
-	fmt.Printf("rs:%v, len:%d, cap:%d\n", rs, len(rs), cap(rs))
-
+	//fmt.Printf("rs:%v, len:%d, cap:%d\n", rs, len(rs), cap(rs))
 	s2 := s[index+1:]
-	fmt.Printf("s2:%v, len:%d, cap:%d\n", s2, len(s2), cap(s2))
+	//fmt.Printf("s2:%v, len:%d, cap:%d\n", s2, len(s2), cap(s2))
 
 	rs = append(rs, s2...)
-	newSlice := make([]int, len(rs), len(rs))
+
+	newSlice := make([]T, len(rs), len(rs))
 	copy(newSlice, rs)
 
 	println("传出容量:", cap(newSlice), "传出长度:", len(newSlice))
 	return newSlice
+}
+
+type collect interface {
+	~int | float64 | float32 | byte | string
 }
--- main.go
+++ main.go
@@ -11,4 +11,17 @@ func main() {
 
 	s1 = SliceOperate.SliceDel(2, s1)
 	fmt.Printf("s1:%v, len:%d, cap:%d\n", s1, len(s1), cap(s1))
+
+	s2 := []string{
+		"aaa",
+		"bbb",
+		"ccc",
+		"ddd",
+	}
+	s2 = SliceOperate.SliceDel(3, s2)
+	fmt.Printf("s2:%v, len:%d, cap:%d\n", s2, len(s2), cap(s2))
+
+	s3 := []byte{'a', 2, 4, 'g', 'v', ';'}
+	s3 = SliceOperate.SliceDel(4, s3)
+	fmt.Printf("s3:%v, len:%d, cap:%d\n", s3, len(s3), cap(s3))
 }
